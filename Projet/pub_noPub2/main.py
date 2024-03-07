20a584e26e39d9903823c3080a380b6807477ad5
armantys <ludovic.souquet@gmail.com>
2024-03-07 16:59:13 +0100

    main

--- Projet/pub_noPub2/main.py
+++ Projet/pub_noPub2/main.py
@@ -1,44 +1,60 @@
-import time  # Importer le module time pour utiliser la fonction sleep
+import datetime
+import os
+import time
 import connexion_firebase
 from snapshot import enregistrer_snapshot
+import cv2
+import tensorflow as tf
+from train import predict_images, load_or_train_model
 
 latest_acquisition = connexion_firebase.get_latest_acquisition()
 latest_application = connexion_firebase.get_latest_application()
 latest_model_IA = connexion_firebase.get_latest_model_IA()
+date_du_jour = datetime.date.today().strftime("%Y-%m-%d")
 
-# Utilisez les données récupérées selon vos besoins
 print("Dernière acquisition:", latest_acquisition)
 print("Dernière application:", latest_application)
 print("Dernier modèle IA:", latest_model_IA)
 
+snapshot_folder = os.path.join(os.getcwd(), 'snapshot')
+
+# Assurez-vous que le répertoire datedujour existe. S'il n'existe pas, créez-le.
+if not os.path.exists(snapshot_folder):
+    os.makedirs(snapshot_folder)
+
+filename_prefix = os.path.join(snapshot_folder, 'snapshot')
+
 def main():
-    # Récupérer les données de la dernière acquisition depuis Firebase
-    latest_acquisition = connexion_firebase.get_latest_acquisition()
+    global snapshot_folder
+    model = load_or_train_model()
 
-    # Vérifier si les données ont été récupérées avec succès
-    if latest_acquisition:
-        # Récupérer la durée de la salve et le nombre d'images
-        duree_salve, nb_images = latest_acquisition
+    if model:
+        latest_acquisition = connexion_firebase.get_latest_acquisition()
 
-        adresse_ip = '192.168.20.37'  # Adresse IP de votre caméra IP
-        port = 88  # Port par défaut pour la plupart des caméras IP
-        nom_utilisateur = 'dev_IA_P3'  # Nom d'utilisateur de la caméra
-        mot_de_passe = 'dev_IA_P3'  # Mot de passe de la caméra
+        if latest_acquisition:
+            duree_salve, nb_images = latest_acquisition
 
-        # Calculer le délai entre chaque enregistrement d'image
-        delai_entre_images = duree_salve / nb_images
+            adresse_ip = '192.168.20.37'
+            port = 88
+            nom_utilisateur = 'dev_IA_P3'
+            mot_de_passe = 'dev_IA_P3'
 
-        # Appel de la fonction pour enregistrer chaque snapshot
-        for i in range(nb_images):
-            # Construire le nom de fichier avec un indice unique
-            filename = f'snapshot\snapshot_{duree_salve}_{i}.jpg'
-            # Appel de la fonction pour enregistrer un snapshot
-            enregistrer_snapshot(adresse_ip, port, nom_utilisateur, mot_de_passe, duree_salve, 1, filename)
-            print(f"Snapshot {i+1}/{nb_images} enregistré avec succès!")
-            # Attendre le délai entre chaque enregistrement
-            time.sleep(delai_entre_images)
+            # Appeler la fonction pour capturer et enregistrer les images
+            enregistrer_snapshot(adresse_ip, port, nom_utilisateur, mot_de_passe, duree_salve, nb_images)
+
+            # Charger les images enregistrées à partir du dossier de sauvegarde
+            snapshot_subfolder = os.path.join(snapshot_folder, date_du_jour)
+            image_filenames = [os.path.join(snapshot_subfolder, f'snapshot_{i}.jpg') for i in range(nb_images)]
+            images = [cv2.imread(filename) for filename in image_filenames]
+
+            # Effectuer des opérations sur les images, telles que la prédiction
+            predictions = predict_images(images)
+            print("Prédictions:", predictions)
+
+        else:
+            print("Aucune acquisition trouvée dans la base de données.")
     else:
-        print("Aucune acquisition trouvée dans la base de données.")
+        print("Impossible de continuer car le modèle n'a pas été chargé avec succès.")
 
 if __name__ == "__main__":
     main()
--- Projet/pub_noPub2/snapshot.py
+++ Projet/pub_noPub2/snapshot.py
@@ -1,18 +1,30 @@
 import requests
 from requests.auth import HTTPDigestAuth
+import datetime
+import os
 
-def enregistrer_snapshot(adresse_ip, port, nom_utilisateur, mot_de_passe, duree_salve, nb_images, filename_prefix):
+def enregistrer_snapshot(adresse_ip, port, nom_utilisateur, mot_de_passe, duree_salve, nb_images_par_salve):
+    # Créer le chemin complet du dossier avec la date actuelle
+    date_du_jour = datetime.date.today().strftime("%Y-%m-%d")
+    snapshot_folder = os.path.join(os.getcwd(), 'snapshot', date_du_jour)
+    
+    # Assurez-vous que le répertoire datedujour existe. S'il n'existe pas, créez-le.
+    if not os.path.exists(snapshot_folder):
+        os.makedirs(snapshot_folder)
+
     # URL du flux vidéo de la caméra
     url_flux_video = f'http://{adresse_ip}:{port}/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr={nom_utilisateur}&pwd={mot_de_passe}'
 
     # Boucle pour enregistrer chaque snapshot
-    for i in range(nb_images):
-        filename = f"{filename_prefix}_{duree_salve}_{i}.jpg"  # Construire le nom de fichier correctement
+    for i in range(nb_images_par_salve):
+        # Construire le nom de fichier avec un index unique
+        filename = f"snapshot_{i+1}.jpg"  # Utilisation de l'index pour générer un nom de fichier unique
+        filepath = os.path.join(snapshot_folder, filename)
         # Récupérer l'image du flux vidéo de la caméra
         response = requests.get(url_flux_video, auth=HTTPDigestAuth(nom_utilisateur, mot_de_passe), stream=True)
         if response.status_code == 200:
-            with open(filename, 'wb') as f:
+            with open(filepath, 'wb') as f:
                 f.write(response.content)
-            print(f"Snapshot {i+1}/{nb_images} enregistré avec succès!")
-        else:
-            print("Impossible de récupérer l'image")
+            print(f"Snapshot {i+1}/{nb_images_par_salve} enregistré avec succès dans {snapshot_folder}!")
+
+    print(f"Tous les snapshots ont été enregistrés avec succès dans {snapshot_folder}!")
